5efa7bbbecd380b9e84bfcc84dae4cbef932c7ab
justin lawrence <justinmarkhamlawrence@gmail.com>
2021-11-27 12:03:36 -0500

    Initial rendering

---
+++ my-app/src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+//if (module.hot){
+//    module.hot,accept();
+//}
+
+const App = () => {
+   return <div>Hi there!</div>
+};
+
+ReactDOM.render(
+    <App />,
+    document.querySelector('#root')
+)
