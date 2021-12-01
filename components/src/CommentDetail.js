b8844ce3ee86f65a3c8b5bff15bda67c56b75635
Justin Lawrence <justinmarkhamlawrence@gmail.com>
2021-11-30 20:03:39 -0500

    Fixing images

--- components/src/CommentDetail.js
+++ components/src/CommentDetail.js
@@ -4,16 +4,16 @@ const CommentDetail = props => {
   return (
     <div className="comments">
         <a href="/" className="avatar">
-          <img alt="avatar pic" ></img>
+          <img alt="avatar pic" src={props.avatar}></img>
         </a>
         <div className="content">
           <a href="/" className="author">
             {props.author}
           </a>
           <div className="metadata">
-            <span className="date">Today at 6:00PM</span>
+            <span className="date">{props.timeAgo}</span>
           </div>
-          <div className="text">Nice blog post!</div>
+          <div className="text">{props.comment}</div>
         </div>
       </div>  
   );
--- components/src/index.js
+++ components/src/index.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import CommentDetail from './CommentDetail';
+import logo from './avatars/ava4.jpeg';
+import logo3 from './avatars/ava3.jpeg';
+import logo2 from './avatars/ava2.jpeg';
 
 const App = () => {
   return (
     <div className="ui container comments">
-      <CommentDetail author="Mika"/>
-      <CommentDetail author="Wade"/>
-      <CommentDetail author="Jack"/>
+      <CommentDetail author="Mika" timeAgo="2 days ago at 5:17" comment="Nice!" avatar={logo}/>
+      <CommentDetail author="Wade" timeAgo="yesterday at 7:48" comment="Rad!!" avatar={logo2}/>
+      <CommentDetail author="Jack" timeAgo="yesterday at 2:00" comment="Murica!" avatar={logo3}/>
     </div>
   );  
 };
